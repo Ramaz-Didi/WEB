f1253fc9268ed5e1629ea83bf7cd23a37035f347
Ramaz-Didi <117308754+Ramaz-Didi@users.noreply.github.com>
2024-11-08 22:01:34 +0000

    add_pandas

--- pages/main.py
+++ pages/main.py
@@ -1,15 +1,17 @@
 import streamlit as st
 from PIL import Image
+import pandas
 
-#image1 = Image.open('photo1.jpg')
+df=pandas.read_csv("data.csv",sep=";")
+image1 = Image.open('photo1.jpg')
 
 #setting container size
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
-#with col1:
-#   st.image(image1)
+with col1:
+   st.image(image1)
 
 with col2:
     st.title("Ardit Sulce")
@@ -17,4 +19,17 @@ with col2:
     Hi it's me
     """
     st.info(content)
-    
+content2 = """
+below comment
+"""
+st.write(content2)
+
+col3, col4 = st.columns(2)
+
+with col3:
+    for index, row in df[:10].iterrows():
+       st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+       st.header(row["title"])
