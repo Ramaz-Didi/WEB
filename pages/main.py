8ebdfbc536e1c8edd7d375f64980f21be77c19ba
Ramaz-Didi <117308754+Ramaz-Didi@users.noreply.github.com>
2024-11-08 21:44:08 +0000

    adding comment

--- pages/main.py
+++ pages/main.py
@@ -1,7 +1,20 @@
 import streamlit as st
+from PIL import Image
+
+#image1 = Image.open('photo1.jpg')
+
+#setting container size
+st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
-with col1:
-    st.image("..\images\photo.png")
+#with col1:
+#   st.image(image1)
+
+with col2:
+    st.title("Ardit Sulce")
+    content ="""
+    Hi it's me
+    """
+    st.info(content)
     
